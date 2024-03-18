2d4727f03449880876f63831277ebdb0589982a1
byteboss125 <byteboss125@gmail.com>
2024-03-18 17:00:46 -0300

    fix: remove irregular space

--- src/components/base/Hero/Hero.tsx
+++ src/components/base/Hero/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
       <p className="max-w-[620px]">
         All pieces are handmade in our workshop and produced on a made to order
         basis. Please keep in mind that once your order is placed production
-        times can be from 8 - 10 weeks. Download full catalog here.
+        times can be from 8 - 10 weeks. Download full catalog here.
       </p>
     </div>
   );
