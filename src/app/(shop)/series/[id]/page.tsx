e78fb9a00875ac20dd53f8ab0f1d3fcd2336b58f
byteboss125 <byteboss125@gmail.com>
2024-03-18 15:27:35 -0300

    feat: add 404 page

--- src/app/(shop)/series/[id]/page.tsx
+++ src/app/(shop)/series/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function SeriesPage({}) {
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function SeriesPage({ params }: Props) {
+  const { id } = params;
+
+  if (id === 'abc') {
+    notFound();
+  }
   return (
     <div className="">
-      <h1>SERIES page</h1>
+      <h1>SERIES page {id}</h1>
     </div>
   );
 }
