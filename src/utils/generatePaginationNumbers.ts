7493dc43087982a6d9278005824dab996379a589
byteboss125 <byteboss125@gmail.com>
2024-03-20 19:48:21 -0300

    feat: add pagination utils function

---
+++ src/utils/generatePaginationNumbers.ts
@@ -0,0 +1,23 @@
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  if (totalPages <= 5) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  if (currentPage <= 3) {
+    return [1, 2, 3, '...', totalPages - 1, totalPages];
+  }
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  return [
+    1,
+    '...',
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    '...',
+    totalPages,
+  ];
+};
