d94245500205457e37fab0d0fb16e2064359c7d5
byteboss125 <byteboss125@gmail.com>
2024-03-21 00:58:06 -0300

    chore: remove unused tag

--- src/components/base/Navbar/Navbar.tsx
+++ src/components/base/Navbar/Navbar.tsx
@@ -8,35 +8,33 @@ export const Navbar = () => {
   const { openSideMenu } = useUiStore();
   const { series } = useParams();
   return (
-    <div className="sticky top-0  z-10">
-      <nav className="py-4 px-2 flex items-center justify-between md:px-4 xl:px-6 border-b border-black  bg-white">
-        <Link href="/" className="font-black text-2xl font-sans md:text-5xl">
-          enkelhet
-        </Link>
-        <ul className="hidden md:flex items-center gap-7">
-          {navItems.map(({ id, label }) => (
-            <li key={id}>
-              <Link
-                href={`/series/${label}`}
-                className={`hover:underline underline-offset-2 ${series === label ? 'underline' : ''}`}
-              >
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className="flex items-center gap-4 text-sm md:gap-6 md:text-base">
-          <li>
-            <button>SEARCH</button>
+    <nav className="py-4 px-2 flex items-center justify-between md:px-4 xl:px-6 border-b border-black  bg-white sticky top-0  z-10">
+      <Link href="/" className="font-black text-2xl font-sans md:text-5xl">
+        enkelhet
+      </Link>
+      <ul className="hidden md:flex items-center gap-7">
+        {navItems.map(({ id, label }) => (
+          <li key={id}>
+            <Link
+              href={`/series/${label}`}
+              className={`hover:underline underline-offset-2 ${series === label ? 'underline' : ''}`}
+            >
+              {label}
+            </Link>
           </li>
-          <li>
-            <Link href={`/cart`}>CART (0)</Link>
-          </li>
-          <li>
-            <button onClick={openSideMenu}>MENU</button>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        ))}
+      </ul>
+      <ul className="flex items-center gap-4 text-sm md:gap-6 md:text-base">
+        <li>
+          <button>SEARCH</button>
+        </li>
+        <li>
+          <Link href={`/cart`}>CART (0)</Link>
+        </li>
+        <li>
+          <button onClick={openSideMenu}>MENU</button>
+        </li>
+      </ul>
+    </nav>
   );
 };
