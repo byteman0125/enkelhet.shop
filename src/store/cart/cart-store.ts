b4e7b999fcfbcad33559eddcb9da9c9a7b9d9eac
byteboss125 <byteboss125@gmail.com>
2024-03-22 13:36:19 -0300

    feat: add middleware in order to make cart data persistant

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,37 +1,45 @@
 import { ICartProduct } from '@/interfaces';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface State {
   cart: ICartProduct[];
   addToCart: (product: ICartProduct) => void;
 }
 
-export const useCartStore = create<State>()((set, get) => ({
-  cart: [],
-  addToCart: (product: ICartProduct) => {
-    const { cart } = get();
-    const productInCart = cart.some(
-      (item) => item.id === product.id && item.finish === product.finish
-    );
-    if (!productInCart) {
-      set({
-        cart: [...cart, product],
-      });
-      return;
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      addToCart: (product: ICartProduct) => {
+        const { cart } = get();
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.finish === product.finish
+        );
+        if (!productInCart) {
+          set({
+            cart: [...cart, product],
+          });
+          return;
+        }
+
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.finish === product.finish) {
+            return {
+              ...item,
+              quantity: item.quantity + product.quantity,
+            };
+          }
+          return item;
+        });
+
+        set({
+          cart: updatedCartProducts,
+        });
+      },
+    }),
+    {
+      name: 'shopping-cart',
     }
-
-    const updatedCartProducts = cart.map((item) => {
-      if (item.id === product.id && item.finish === product.finish) {
-        return {
-          ...item,
-          quantity: item.quantity + product.quantity,
-        };
-      }
-      return item;
-    });
-
-    set({
-      cart: updatedCartProducts,
-    });
-  },
-}));
+  )
+);
