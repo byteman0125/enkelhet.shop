a8a7c5903812edf1eba73d451327765a7ca430e7
byteboss125 <byteboss125@gmail.com>
2024-03-24 00:22:21 -0300

    feat: configure basic credentials

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,24 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/register',
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        //search email
+
+        //compare password
+
+        return null;
+      },
+    }),
+  ],
 };
