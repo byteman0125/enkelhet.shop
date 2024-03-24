c150440f31ad2683dad9ba4455dc25d958bc522b
byteboss125 <byteboss125@gmail.com>
2024-03-24 00:38:59 -0300

    feat: connect login with auth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,7 @@
 import type { NextAuthConfig } from 'next-auth';
+import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
 
 export const authConfig: NextAuthConfig = {
   pages: {
@@ -13,12 +15,19 @@ export const authConfig: NextAuthConfig = {
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
 
+        if (!parsedCredentials.success) return null;
+        const { email, password } = parsedCredentials.data;
+        console.log({ email, password });
         //search email
 
         //compare password
+
+        //return user data
 
         return null;
       },
     }),
   ],
 };
+
+export const { signIn, signOut, auth: middleware } = NextAuth(authConfig);
