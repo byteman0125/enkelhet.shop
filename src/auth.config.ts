c29912ca4a7ecfd59eba8d07a3061f44bd1c2632
byteboss125 <byteboss125@gmail.com>
2024-03-24 00:17:53 -0300

    feat: add next auth config file

---
+++ src/auth.config.ts
@@ -0,0 +1,9 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/register',
+  },
+  providers: [],
+};
