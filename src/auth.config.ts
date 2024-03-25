43484325323535e4e58c3f5254c4b5efed5de7b5
byteboss125 <byteboss125@gmail.com>
2024-03-25 12:49:09 -0300

    feat: extend user data

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,15 +1,44 @@
 import bcryptjs from 'bcryptjs';
-import type { NextAuthConfig } from 'next-auth';
-import NextAuth from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+
 import prisma from './utils/prisma';
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/auth/login',
-    newUser: '/auth/register',
+    newUser: '/auth/new-account',
   },
+
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      // const isLoggedIn = !!auth?.user;
+
+      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL('/dashboard', nextUrl));
+      // }
+      return true;
+    },
+
+    jwt({ token, user }) {
+      if (user) {
+        token.data = user;
+      }
+
+      return token;
+    },
+
+    session({ session, token, user }) {
+      session.user = token.data as any;
+      return session;
+    },
+  },
+
   providers: [
     Credentials({
       async authorize(credentials) {
@@ -20,12 +49,17 @@ export const authConfig: NextAuthConfig = {
         if (!parsedCredentials.success) return null;
 
         const { email, password } = parsedCredentials.data;
+
+        // Buscar el correo
         const user = await prisma.user.findUnique({
           where: { email: email.toLowerCase() },
         });
         if (!user) return null;
+
+        // Comparar las contraseñas
         if (!bcryptjs.compareSync(password, user.password)) return null;
 
+        // Regresar el usuario sin el password
         const { password: _, ...rest } = user;
 
         return rest;
