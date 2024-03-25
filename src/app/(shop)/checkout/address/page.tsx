4153881a66a3a77dad16a10d22985e15f2c7d11d
byteboss125 <byteboss125@gmail.com>
2024-03-25 19:27:25 -0300

    feat: connect form validations

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,67 +1,12 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { AddressForm } from './ui/AddressForm';
 
 export default function AddressPage() {
   return (
     <>
       <p className="py-4 px-2 md:px-4 xl:px-6">SHIPPING ADDRESS</p>
       <div className="w-full grid grid-cols-1 xl:grid-cols-2 border-t border-black">
-        <div className="w-full h-full py-4 px-2 md:px-4 xl:px-6 flex flex-col gap-5">
-          <div className="flex flex-col gap-2">
-            <h2 className="text-xl">Personal info</h2>
-            <div className="grid grid-cols-2 border border-black">
-              <input
-                type="text"
-                placeholder="First Name"
-                className="border-r border-black p-3"
-              />
-              <input type="text" placeholder="Last Name" className="p-3" />
-              <input
-                type="text"
-                placeholder="Phone number"
-                className="p-3 border-t border-black col-span-2"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h2 className="text-xl">Shipping address info</h2>
-            <div className="grid grid-cols-2 border border-black">
-              <input
-                type="text"
-                placeholder="Address"
-                className="border-r border-black p-3"
-              />
-              <input
-                type="text"
-                placeholder="Second address (optional)"
-                className="p-3"
-              />
-              <input
-                type="text"
-                placeholder="ZIP code"
-                className="border-t border-r border-black p-3"
-              />
-              <input
-                type="text"
-                placeholder="City"
-                className="p-3 border-t border-black"
-              />
-              <input
-                type="text"
-                placeholder="Country"
-                className="p-3 border-t border-black col-span-2"
-              />
-            </div>
-            <div className="w-full flex items-center mt-8">
-              <Link
-                href={`/checkout`}
-                className="flex items-center justify-center col-span-2 md:col-span-3 w-full bg-black px-4 py-4 text-white text-sm md:text-base "
-              >
-                Continue to summary
-              </Link>
-            </div>
-          </div>
-        </div>
+        <AddressForm />
         <div className="hidden xl:block w-full h-[calc(100vh-81px-56px)] relative">
           <Image
             src={`/address-image.jpg`}
---
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -0,0 +1,109 @@
+'use client';
+import { useForm } from 'react-hook-form';
+
+type FormInputs = {
+  firstName: string;
+  lastName: string;
+  address: string;
+  address2?: string;
+  postalCode: string;
+  city: string;
+  country: string;
+  phone: string;
+  rememberAddress?: boolean;
+};
+
+export const AddressForm = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { isValid },
+  } = useForm<FormInputs>();
+
+  const onSubmit = (data: FormInputs) => {
+    console.log({ data });
+  };
+
+  return (
+    <form
+      className="w-full h-full py-4 px-2 md:px-4 xl:px-6 flex flex-col gap-5"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl">Personal info</h2>
+        <div className="grid grid-cols-2 border border-black outline-none">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="border-r border-black p-3 outline-none"
+            {...register('firstName', { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="p-3 outline-none"
+            {...register('lastName', { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="Phone number"
+            className="p-3 border-t border-black col-span-2 outline-none"
+            {...register('phone', { required: true })}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl">Shipping address info</h2>
+        <div className="grid grid-cols-2 border border-black outline-none">
+          <input
+            type="text"
+            placeholder="Address"
+            className="border-r border-black p-3 outline-none"
+            {...register('address', { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="Second address (optional)"
+            className="p-3 outline-none"
+            {...register('address2')}
+          />
+          <input
+            type="text"
+            placeholder="ZIP code"
+            className="border-t border-r border-black p-3 outline-none"
+            {...register('postalCode', { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="City"
+            className="p-3 border-t border-black outline-none"
+            {...register('city', { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            className="p-3 border-t border-black col-span-2 outline-none"
+            {...register('country', { required: true })}
+          />
+        </div>
+        <div className="flex items-center gap-3 mt-8">
+          <input
+            type="checkbox"
+            id="checkbox"
+            {...register('rememberAddress')}
+          />
+          <label htmlFor="checkbox">Do you want to save the address?</label>
+        </div>
+        <div className="w-full flex items-center mt-8">
+          <button
+            disabled={!isValid}
+            type="submit"
+            className={`flex items-center justify-center col-span-2 md:col-span-3 w-full px-4 py-4 text-white text-sm md:text-base ${isValid ? 'bg-black' : 'bg-gray-500'}`}
+          >
+            Continue to summary
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
