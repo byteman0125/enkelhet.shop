97a3225531bf9ed6721c1629447798b2c5d81379
byteboss125 <byteboss125@gmail.com>
2024-03-26 00:39:57 -0300

    feat: add user address delete function

---
+++ src/actions/address/delete-user-address.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import prisma from '@/utils/prisma';
+
+export const deleteUserAddress = async (userId: string) => {
+  try {
+    const deleted = await prisma.userAddress.delete({
+      where: {
+        userId,
+      },
+    });
+
+    return { ok: true };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'Address delete failed',
+    };
+  }
+};
