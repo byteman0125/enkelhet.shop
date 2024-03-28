5aa183452d77a89672dca619728aee019a9194a4
byteboss125 <byteboss125@gmail.com>
2024-03-28 13:37:22 -0300

    feat: add paypal keys

--- prisma/migrations/20240328025617_ispaid/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isPayed` on the `Order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Order" DROP COLUMN "isPayed",
-ADD COLUMN     "isPaid" BOOLEAN NOT NULL DEFAULT false;
