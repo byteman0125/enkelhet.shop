95d4869a5d4cc24807946ccd28da8f1f0770bde7
byteboss125 <byteboss125@gmail.com>
2024-03-27 23:18:09 -0300

    feat: get total, subtotal and taxes server action

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from '@/auth.config';
 import type { IAddress } from '@/interfaces';
+import prisma from '@/utils/prisma';
 import { Finish } from '@prisma/client';
 
 interface IProductToOrder {
@@ -23,6 +24,33 @@ export const placeOrder = async (
       message: 'no session available',
     };
   }
-  console.log({ productIds, address, userId });
-  return { productIds, address, userId };
+
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((p) => p.productId),
+      },
+    },
+  });
+
+  const itemsinOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((product) => product.id === item.productId);
+
+      if (!product) throw new Error(`${item.productId} doesn't exist - 500`);
+
+      const subTotal = product.price * productQuantity;
+
+      totals.subTotal += subTotal;
+      totals.tax += subTotal * 0.15;
+      totals.total += subTotal * 1.15;
+
+      return totals;
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  );
+
+  console.log({ subTotal, total, tax });
 };
