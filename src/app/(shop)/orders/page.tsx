d7370c64b950c98928760e6ec9772952ba5c3198
byteboss125 <byteboss125@gmail.com>
2024-05-23 12:52:31 -0300

    feat: improve responsiveness in user orders

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -7,23 +7,23 @@ export default async function OrdersPage() {
 
   return (
     <>
-      <div className="py-4 px-2 md:px-4 xl:px-6 flex items-center justify-between w-full sticky top-[81px] z-10 bg-white border-b border-black">
+      <div className="py-4 px-2 md:px-4 xl:px-6 flex items-center justify-between w-full sticky top-[65px] md:top-[81px] z-10 bg-white border-b border-black">
         <p>ORDERS</p>
       </div>
       <div className="relative overflow-x-auto">
         <table className="w-full text-left rtl:text-right">
           <thead className="uppercase  py-4 px-2 md:px-4 xl:px-6 border-b border-black bg-black text-white">
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 whitespace-nowrap">
                 Product ID
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 whitespace-nowrap">
                 Full Name
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 whitespace-nowrap">
                 Payment status
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 whitespace-nowrap">
                 options
               </th>
             </tr>
@@ -37,19 +37,19 @@ export default async function OrdersPage() {
                 >
                   {order.id}
                 </th>
-                <td className="px-6 py-4">
+                <td className="px-6 py-4 whitespace-nowrap">
                   {order.OrderAddress!.firstName} {order.OrderAddress!.lastName}
                 </td>
                 <td className={`px-6 py-4 `}>
                   <p
-                    className={`${order!.isPaid ? 'bg-green-400' : 'bg-red-400'} w-fit`}
+                    className={`${order!.isPaid ? 'bg-green-400' : 'bg-red-400'} w-fit whitespace-nowrap`}
                   >
                     {order!.isPaid
                       ? 'The order is already paid'
                       : 'Pending payment'}
                   </p>
                 </td>
-                <td className="px-6 py-4">
+                <td className="px-6 py-4 whitespace-nowrap">
                   <Link
                     href={`/orders/${order.id}`}
                     className="hover:underline underline-offset-2"
